84fe3436c9366ed31ae635d43a42aa5235f0fd6f
ice-blue-lp <lp19970628@163.com>
2020-03-04 15:14:44 +0800

    分词

--- demo/preprocessing.py
+++ demo/preprocessing.py
@@ -5,7 +5,7 @@ import jieba as jb
 def stop_words():
     """
     停用词
-    :return:
+    :return:stop_words
     """
     with open("stopwords/stop_words") as fr:
         stop_words = set([word.strip() for word in fr])
@@ -17,8 +17,11 @@ if __name__ == '__main__':
     # 加载停用词
     stop_words = stop_words()
 
+    # 读取训练集数据
     df = pd.read_csv("data/train.csv", sep="\t")
 
+    # 分词并去除停用词
     df["comment"] = df["comment"].map(lambda x: " ".join([i for i in jb.cut(x) if i not in stop_words]))
 
+    # 保存文件，成新的训练集
     df.to_csv("./o2ocomment.csv", index=False, header=False, columns=["label", "comment"])
